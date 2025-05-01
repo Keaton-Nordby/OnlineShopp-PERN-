86243a1c266fadbe5de97f1c2809a59b0180cc78
Keaton Nordby <keatonnordby4@gmail.com>
2025-05-01 16:11:36 -0500

    db working now

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
+
+// creates a sql connection using our variables
 export const sql = neon(
     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 )
+
+// now this sql functio we will export will be used as a tagged template for literal, which allows us to write sql queries safely
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { sql } from "./config/db.js";
+
+
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -20,6 +23,29 @@ app.use(morgan("dev")); // logs requests to the console -> also the first time u
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-});
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10,2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+
+            )
+        `;
+
+        console.log("DB running successfully");
+    } catch (error) {
+        console.log("Error initDb", error);
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port ' + PORT);
+    });
+
+})
