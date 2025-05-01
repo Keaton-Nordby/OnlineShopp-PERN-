43bf4d909613d1b1b48c556f815371c3c98c16bd
Keaton Nordby <keatonnordby4@gmail.com>
2025-05-01 15:25:48 -0500

    db setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+console.log(PORT);
+
+app.use(express.json());
+app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps protect the app by setting various HTTP headers -> this is my first time using this
-app.use(morgan("dev")); // this tool helps log the requests to the console -> also the first time using this
+app.use(morgan("dev")); // logs requests to the console -> also the first time using this
 
-app.get("/test",  (req, res) => {
-    res.send("Hello from the test route.");
-});
+app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
 });
